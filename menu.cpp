3a72fa1bc2d4aa199529853e957eb2141ac0da2e
Akhmad Hafiz Azhari <hafiz060905@gmail.com>
2025-05-06 15:04:08 +0800

    Menambahkan kata kedalam projek

--- menu.cpp
+++ menu.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 void dMenu(){
 system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
+cout<<"Aplikasi sorrting bubble"<<"\n";       
+cout<<"1. Mesukan data"<<"\n";            
+cout<<"2. Menampilkan data"<<"\n";            
+cout<<"3. Sorting"<<"\n";           
+cout<<"4. Sepatah Kata"<<"\n";            
 cout<<"5. Exit"<<"\n";           
 cout<<"Masukan angka :";        
 
@@ -21,6 +21,11 @@ cout<<"hallo saya menu "<<pesan;
 getch();
 }
 
+void Sepatah_Kata(){
+  system("cls");
+  cout <<"indonesia Bisa, Kita Juara\n @poliban";
+  getch();
+}
 
 int main() {
 char pl;
@@ -45,6 +50,7 @@ do
    case '4':
     mPertama("ke- empat");
     /* code */
+    Sepatah_Kata(); 
     break;  
   case '5':
     /* code */
