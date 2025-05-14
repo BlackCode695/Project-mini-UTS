a66f9072438746f2064b2539f357db512338617e
Akhmad Hafiz Azhari <hafiz060905@gmail.com>
2025-05-14 23:54:08 +0800

    Menambahkan menu fungsi untuk input dari user dan metode sorting ascending dan desending

--- menu.cpp
+++ menu.cpp
@@ -1,69 +1,160 @@
 #include <iostream>
 #include <conio.h>
+#include <vector>
 using namespace std;
 
+// Deklarasi variabel global
+vector<int> userData;
 
 void dMenu(){
-system("cls");
-cout<<"Aplikasi sorrting bubble"<<"\n";       
-cout<<"1. Mesukan data"<<"\n";            
-cout<<"2. Menampilkan data"<<"\n";            
-cout<<"3. Sorting"<<"\n";           
-cout<<"4. Sepatah Kata"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
+    system("cls");
+    cout << "Aplikasi Sorting Insertion" << "\n";       
+    cout << "1. Masukan data" << "\n";            
+    cout << "2. Menampilkan data" << "\n";            
+    cout << "3. Sorting" << "\n";           
+    cout << "4. Sepatah Kata" << "\n";            
+    cout << "5. Exit" << "\n";           
+    cout << "Masukan angka :";        
 }
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
+void inputData(){
+    system("cls");
+    userData.clear(); // Menghapus data lama jika ada
+    
+    int jumlah;
+    cout << "Masukkan jumlah data yang ingin diinput: ";
+    cin >> jumlah;
+    
+    for(int i = 0; i < jumlah; i++){
+        int nilai;
+        cout << "Masukkan data ke-" << (i+1) << ": ";
+        cin >> nilai;
+        userData.push_back(nilai);
+    }
+    
+    cout << "\nData berhasil disimpan!" << endl;
+    cout << "Tekan apa saja untuk kembali ke menu...";
+    getch();
+}
+
+void tampilkanData(){
+    system("cls");
+    
+    if(userData.empty()){
+        cout << "Data masih kosong. Silakan input data terlebih dahulu." << endl;
+    } else {
+        cout << "Berikut adalah data yang telah diinputkan:" << endl;
+        for(int i = 0; i < userData.size(); i++){
+            cout << "Data ke-" << (i+1) << ": " << userData[i] << endl;
+        }
+    }
+    
+    cout << "\nTekan apa saja untuk kembali ke menu...";
+    getch();
+}
+
+void insertionSortAscending(vector<int>& arr){
+    int n = arr.size();
+    for(int i = 1; i < n; i++){
+        int key = arr[i];
+        int j = i - 1;
+        
+        while(j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
+
+void insertionSortDescending(vector<int>& arr){
+    int n = arr.size();
+    for(int i = 1; i < n; i++){
+        int key = arr[i];
+        int j = i - 1;
+        
+        while(j >= 0 && arr[j] < key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+}
+
+void sortingData(){
+    system("cls");
+    
+    if(userData.empty()){
+        cout << "Data masih kosong. Silakan input data terlebih dahulu." << endl;
+    } else {
+        char pilihan;
+        cout << "Pilih metode sorting:" << endl;
+        cout << "A. Ascending (kecil ke besar)" << endl;
+        cout << "B. Descending (besar ke kecil)" << endl;
+        cout << "Pilihan Anda (A/B): ";
+        cin >> pilihan;
+        
+        vector<int> sortedData = userData; // Membuat salinan data
+        
+        if(pilihan == 'A' || pilihan == 'a'){
+            insertionSortAscending(sortedData);
+            cout << "\nHasil sorting secara Ascending:" << endl;
+        } else if(pilihan == 'B' || pilihan == 'b'){
+            insertionSortDescending(sortedData);
+            cout << "\nHasil sorting secara Descending:" << endl;
+        } else {
+            cout << "\nPilihan tidak valid. Kembali ke menu utama." << endl;
+            getch();
+            return;
+        }
+        
+        // Menampilkan hasil sorting
+        for(int i = 0; i < sortedData.size(); i++){
+            cout << "Data ke-" << (i+1) << ": " << sortedData[i] << endl;
+        }
+    }
+    
+    cout << "\nTekan apa saja untuk kembali ke menu...";
+    getch();
 }
 
 void Sepatah_Kata(){
-  system("cls");
-  cout <<"indonesia Bisa, Kita Juara\n @poliban";
-  getch();
+    system("cls");
+    cout << "Bismillah Kita Berhasil Membuat Program Sorting!" << endl;
+    cout << "Tekan apa saja untuk kembali ke menu...";
+    getch();
 }
 
 int main() {
-char pl;
-do
-{
-    dMenu();
-    pl=getch();
-  switch (pl)
-  {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    mPertama("ke- empat");
-    /* code */
-    Sepatah_Kata(); 
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
-
-
-} while (pl!='5');
-  return 0;
+    char pl;
+    do {
+        dMenu();
+        pl = getch();
+        switch (pl) {
+            case '1':
+                inputData();
+                break;
+            case '2':
+                tampilkanData();
+                break;
+            case '3':
+                sortingData();
+                break;
+            case '4':
+                Sepatah_Kata();
+                break;
+            case '5':
+                system("cls");
+                cout << "Terima kasih telah menggunakan program ini!" << endl;
+                break;
+            default:
+                system("cls");
+                cout << "Pilihan Tidak Tersedia" << endl;
+                cout << "Tekan apa saja untuk kembali ke menu...";
+                getch();
+                break;
+        }
+    } while (pl != '5');
+    
+    return 0;
 }
